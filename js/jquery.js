15809cb82f72a60263f4f1e2ec7d072b573b4898
Zalupaman <merkishev02@mail.ru>
2019-08-30 12:03:34 +0300

    1.6

---
+++ js/jquery.js
@@ -0,0 +1,63 @@
+$(function () {
+
+    $('#seeresum1').click(function () {
+        $('#resum').slideDown();
+    });
+    $('#close').click(function () {
+        $('#resum').slideUp();
+    });
+
+});
+$(function () {
+
+    $('#seeresum2').click(function () {
+        $('#resum').slideDown();
+    });
+    $('#close').click(function () {
+        $('#resum').slideUp();
+    });
+
+});
+
+$(function () {
+
+    $('#hire-me1').click(function () {
+        $('#request').slideDown();
+    });
+    $('#close-request').click(function () {
+        $('#request').slideUp();
+    });
+
+});
+$(function () {
+
+    $('#hire-me2').click(function () {
+        $('#request').slideDown();
+    });
+    $('#close-request').click(function () {
+        $('#request').slideUp();
+    });
+
+});
+
+$(function () {
+
+    $('#hire-me3').click(function () {
+        $('#request').slideDown();
+    });
+    $('#close-request').click(function () {
+        $('#request').slideUp();
+    });
+
+});
+
+$(function () {
+
+    $('#hire-me4').click(function () {
+        $('#request').slideDown();
+    });
+    $('#close-request').click(function () {
+        $('#request').slideUp();
+    });
+
+});
